088742cf2586527f7323ee1d7a587e187a39f6b0
Eric4044 <125145233+Eric4044@users.noreply.github.com>
2023-04-09 13:48:41 +0800

    Update player.h

--- src/player.h
+++ src/player.h
@@ -1,6 +1,5 @@
 #ifndef PLAYER
 #define PLAYER
-#endif
 
 #ifndef BLOCK
 #include "block.h"
@@ -9,38 +8,66 @@
 
 class Player{
 public:
-    Player();
-    Player(char, int, int, int, int, int);
+    //Constructor
+    Player(char appearance_,int level_,int bombNum_,int wealth_,int damage_, int score_, int depth_, int x_, int y_):
+             appearance(appearance_), level(level_), bombNum(bombNum_), wealth(wealth_),damage(damage_),score(score_),depth(depth_),x(x_),y(y_) {}
+
+    Player(): Player('@',1,1,0,1,0,0,0,0) {}
+    Player(char appearance_, int level_, int bombNum_, int wealth_,int damage_): Player(appearance_,level_,bombNum_,wealth_,damage_,0,0,0,0) {}
+
+    //Destructor
+    ~Player() {}
+
+    //movement
 
     void move_left();
     void move_right();
     void move_down();
+    void move_right(Map &mp);  //Judging whether to the edge
+    void move_down(Map &mp);   //Judging whether to the edge
 
-    void attack(Block &block);
+    //player attack
+
+    void attack(Block &block,auto direction);
     void bomb(Map &mp);
-    
-    //for shop
-    int getWealth() const;
-    void setWealth(int newWealth);
-    char getAppearance() const;
-    void setAppearance(char newAppearance);
-    int getBombNum() const;
-    void setBombNum(int newBombNum);
-    int getAttack() const;
-    void setAttack(int newAttack);
-    
-    //for IO
-    int get_x() 
-    int get_y()
-        
+
+    //getters and setters
+
+    int getWealth();
+    void setWealth(int wealth);
+
+    char getAppearance();
+    void setAppearance(char appearance);
+
+    int getBombNum();
+    void setBombNum(int bombNum);
+
+    int getDamage();
+    void setDamage(int damage);
+
+    int getX();
+    int getY();
+    void setLocation(int x,int y);
+
+    int getScore();
+    void setScore(int score);
+
+    //Reset score, depth, x, y when starting a new game
+    void playerInitialization(Map &mp);
+
 private:
     char appearance;
     int level;
     int bombNum;
     int wealth;
-    int attack;
-    int x, y;
+    int damage;
+    int score;       //current game score
+    int depth;       //absolute depth
+    int x, y;        //location on the map
 
     void move(int);
+
 };
+
+#endif
 
