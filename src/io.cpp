fa6be5c99b94a7faa9a1b911160fde1b8b5eeea1
duanxihan <117086781+duanxihan@users.noreply.github.com>
2023-04-11 12:57:38 +0800

    Update io.cpp
    enable to save and load previous score form file

--- src/io.cpp
+++ src/io.cpp
@@ -16,9 +16,10 @@ int saveToFile(const Player& player){       // Save the player info into gameDat
     if(!out) return 1;              // Fail to open data file.
     auto wealth = player.getWealth(), bomb = player.getBombNum();
     auto appearance = player.getAppearance();
-    out << wealth << "\n" << bomb << "\n" << appearance << "\n";
+    auto score = player.getScore();
+    out << wealth << "\n" << bomb << "\n" << appearance << "\n" << score << "\n";
 
-    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance;          // Join the items into a string
+    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance+std::to_string(score);          // Join the items into a string
     long long sum = encode(info);           // Get the hash sum.
     out << sum;
 
@@ -31,16 +32,18 @@ int loadFromFile(Player& player){
     if(!in) return 1;           // Fail to open data file.
     int wealth, bomb;
     char appearance;
+    int score;
     long long data_sum, cur_sum;
-    in >> wealth >> bomb >> appearance >> data_sum;
+    in >> wealth >> bomb >> appearance >> score >> data_sum;
 
-    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance;          // Join the items into a string
+    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance+std::to_string(score);          // Join the items into a string
     cur_sum = encode(info);
     if(cur_sum != data_sum) return 2;           // Cannot pass hash validation. Data is incorrect.
 
     player.setWealth(wealth);
     player.setBombNum(bomb);
     player.setAppearance(appearance);
+    player.setScore(score)
 
     return 0;           // Load successfully.
 }
