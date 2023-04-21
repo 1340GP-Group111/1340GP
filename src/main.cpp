f22600b1723d40426269eb47fd0a6d2f364e9d1e
St-Box <108875875+St-Box@users.noreply.github.com>
2023-04-21 16:40:56 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,82 +3,91 @@
 #include "io.h"
 #include "player.h"
 #include "shop.h"
-#include "timer.h"
+//#include "timer.h"
 #include "ui.h"
-
-int shop(Player & real_p){    // Shop ��������
-    ShoppingPlayer p(10,5);
-    ShoppingMap map(20,10);
-    map.show_map(p,real_p);
-    while(true){
-        string Input;
-        cin>>Input;
-        if(Input=="quit" || Input=="Quit")
-            return 2;
-        char Input_char=tolower(Input[0]);
-        if(Input_char=='w'){
-            p.move_up(map.mp,real_p);
-        }
-        else if(Input_char=='s'){
-            p.move_down(map.mp,real_p);
-        }
-        else if(Input_char=='a'){
-            p.move_left(map.mp,real_p);
-        }
-        else if(Input_char=='d'){
-            p.move_right(map.mp,real_p);
-        }
-        else if(Input_char=='r'){
-            return 0;
-        }
-
-        map.show_map(p,real_p);
-
-    }
-}
+#include<conio.h> // to be deleted. <conio.h> is for windows
+#include<iostream>
+#include<ctime>
 
 Player p;
 
-int game(){
-    Timer t;
-    while(!t.chk_timeout()){
-        int key = ui::listenKeyboard();
-        // TODO: attack
-        if(key == 's'){
-            p.move_down();
-        }
-        else if(key == 'a'){
-            p.move_left();
-        }
-        else if(key == 'd'){
-            p.move_right();
-        }
-        else if(key == 'r'){
-            return 1;
-        }
-        else if(key == 'q'){
-            return 2;
-        }
-    }
+int Game(Player &p){
+	p.setDepth(0);
+	p.setAppearance('P');	
+	Map mp;
+	p.playerInitialization(mp);
+	time_t start_time = time(NULL);// start counting the time
+	time_t end_time = time(NULL);
+	int lasted_time = end_time-start_time;
+	mp.show_map(p);
+	while(true){ //	             MAIN LOOP OF DIGGING
+		char Input;
+		Input = getch();
+		if (Input == 'r'){
+			return 1;
+		}
+		
+		Input = tolower(Input);
+		if (Input=='a'){
+			p.move_left(mp);
+		}
+		else if(Input=='d'){
+			p.move_right(mp);
+		}
+		else if(Input=='s'){
+			p.move_down(mp);
+		}
+		else if(Input=='b'){
+			p.bomb(mp);
+		}
+		
+		end_time = time(NULL); 		//Checking the remaining time for player
+		lasted_time = end_time-start_time;
+		p.setTime(p.getOxygen() - lasted_time);
+		mp.show_map(p);
+		if(p.getTime()<0){
+			return 3;
+		}
+	}
 }
-
-void gameLoop(){
-    // Game loop.
-    int signal = 0; //use to quit the whole game;
-    while(true){
-        signal = game();
-        if (signal==1){ //1 -- Switch to Shopping
-            signal = shop(p);
-        }
-        if (signal == 2){ //2 -- Quit the whole game
-            return;
-        }
-    }
-}
-
 int main(){
-    // Main menu.
-    ui::init(Map::HEIGHT, Map::WIDTH);
 
+    //Load the game data
+    int load_result = loadFromFile(p);
+	if(load_result == 0){
+		std::cout<<"\n\nLoad data successfully"<<std::endl;
+	}
+	else{
+		std::cout<<"\n\nFailed to load data"<<std::endl;
+	}
+    int height = 20;    	
+    int width = 12;
+	ui::init(height, width);
+	// Main menu.
+    
+	int signal = 0; //use to quit the whole game; 0-Game 1-Shop 2-Quit 3-Time run out
+	while(signal != 2){
+		signal = Game(p);
+		if (signal==1){ //1 -- Switch to Shopping
+			signal = shop(p);
+		}
+		if (signal == 2){ //2 -- Quit the whole game
+			break;
+		}
+		if (signal == 3){
+			std::cout<<"\n\n  Time Run Out! \n\n Type anything to continue \n"<<std::endl;
+			std::string temp;
+			std::cin>>temp;
+			signal = shop(p);
+		}
+	}
+	
+	int save_result = saveToFile(p);	// -- Save the data
+	if (save_result == 0){
+		std::cout<<"Data saved successfully\n\n"<<std::endl;
+	}
+	else{
+		std::cout<<"Failed to save data\n\n"<<std::endl;
+	}
     return 0;
 }
