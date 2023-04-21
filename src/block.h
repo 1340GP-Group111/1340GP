a69c150f3d1b68e88edaec07ace91ae6b6a30cc7
St-Box <108875875+St-Box@users.noreply.github.com>
2023-04-21 16:34:36 +0800

    Update block.h

--- src/block.h
+++ src/block.h
@@ -18,39 +18,40 @@ private:
     int status;
 
 public:   
-    std::string get_id() const;
-    char get_appearance() const;
+    std::string get_id() ;
+    char get_appearance() ;
     char get_breaking_app();
     int get_health();
     int get_max_health();
     int get_value();
-    int get_status() const;
+    int get_status() ;
     
     Block();
     Block(std::string id_, char appearance_, char breaking_app_, int health_, int value_, int status_);
     void copy(Block target);
-    int attack(int damage);           //攻击方块。输入扣血量，如果方块已经是空的返回1, 攻击破坏成功返回2,未破坏返回0 
+    int attack(int damage);           //attack a block. empty - return 1; broken - return 2; not broken - return 0
 };
 
 //health: Linear growth
 //value:  Exponential growth 
 //empty blocks:
-Block b_00("white",'#','*',0,0,0); 
+extern Block b_00; 
 //normal blocks:
-Block b_0("white",'#','*',2,1,1);
-Block b_1("green",'#','*',3,2,1);
-Block b_2("blue",'#','*',4,4,1);
-Block b_3("yellow",'#','*',5,8,1);
-Block b_4("red",'#','*',6,16,1);
-Block b_5("pink",'#','*',7,32,1);
-Block b_6("cyan",'#','*',8,64,1);
+extern Block b_0;
+extern Block b_1;
+extern Block b_2;
+extern Block b_3;
+extern Block b_4;
+extern Block b_5;
+extern Block b_6;
 //special blocks with higher values:
-Block s_0("white",'$','$',1,10,1);
-Block s_1("green",'$','$',1,20,1);
-Block s_2("blue",'$','$',1,40,1);
-Block s_3("yellow",'$','$',1,80,1);
-Block s_4("red",'$','$',1,160,1);
-Block s_5("pink",'$','$',1,320,1);
-Block s_6("cyan",'$','$',1,640,1);
+extern Block s_0;
+extern Block s_1;
+extern Block s_2;
+extern Block s_3;
+extern Block s_4;
+extern Block s_5;
+extern Block s_6;
 //obstacles that cannot be broken:
-Block ob_0("black",'#','*',999,1,1); 
+extern Block ob_0;
+#endif 
