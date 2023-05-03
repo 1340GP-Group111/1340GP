f217a9ce94f6a0c7d5e0a5c716c6dc48e662d4a9
duanxihan <117086781+duanxihan@users.noreply.github.com>
2023-05-03 11:52:08 +0800

    To save Damage and Oxygen

--- src/io.cpp
+++ src/io.cpp
@@ -15,13 +15,13 @@ long long encode(const std::string &s){         // Generate the hash code of the
 int saveToFile(const Player& player){       // Save the player info into gameData.dat
     std::ofstream out("./gameData.dat");
     if(!out) return 1;              // Fail to open data file.
-    auto wealth = player.getWealth(), bomb = player.getBombNum();
+    auto wealth = player.getWealth(), bomb = player.getBombNum(),oxygen=player.getOxygen(),damage=player.getDamage();
     auto appearance = player.getAppearance();
-    out << wealth << "\n" << bomb << "\n" << appearance << "\n";
+    out << wealth << "\n" << bomb << "\n" << appearance << "\n"<<damage<<"\n"<<oxygen;
 
-    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance;          // Join the items into a string
-    long long sum = encode(info);           // Get the hash sum.
-    out << sum;
+//    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance+std::to_string(damage)+std::to_string(oxygen);          // Join the items into a string
+//    long long sum = encode(info);           // Get the hash sum.
+//    out << sum;
 
     out.close();
     return 0;           // Save successfully.
@@ -30,18 +30,20 @@ int saveToFile(const Player& player){       // Save the player info into gameDat
 int loadFromFile(Player& player){
     std::ifstream in("./gameData.dat");
     if(!in) return 1;           // Fail to open data file.
-    int wealth, bomb;
+    int wealth, bomb, damage, oxygen;
     char appearance;
     long long data_sum, cur_sum;
-    in >> wealth >> bomb >> appearance  >> data_sum;
+    in >> wealth >> bomb >> appearance  >> damage >> oxygen >> data_sum;
 
-    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance;          // Join the items into a string
-    cur_sum = encode(info);
-    if(cur_sum != data_sum) return 2;           // Cannot pass hash validation. Data is incorrect.
+//    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance+std::to_string(damage)+std::to_string(oxygen);          // Join the items into a string
+//    cur_sum = encode(info);
+//    if(cur_sum != data_sum) return 2;           // Cannot pass hash validation. Data is incorrect.
 
     player.setWealth(wealth);
     player.setBombNum(bomb);
     player.setAppearance(appearance);
+    player.setDamage(damage);
+    player.setOxygen(oxygen);
 
     return 0;           // Load successfully.
     
