9d55b0732b4ec87c5060481f3d632744b410055a
St-Box <108875875+St-Box@users.noreply.github.com>
2023-05-05 16:24:48 +0800

    Update comments

--- src/gameMap.h
+++ src/gameMap.h
@@ -11,15 +11,11 @@ class Player;
 class Map{
 public:
     int width;
-	int height;
+    int height;
     std::deque<std::vector<Block> > mp;
-
-    Map();
-
-    Map(int, int);
-
-    void generateLine(const Player& p);
-
+    Map();				//default constructor
+    Map(int, int);			//constructor, input the height and width.
+    void generateLine(const Player& p);	//generate a new line according to the player's depth
     void show_map(const Player& p);
 };
 #endif
