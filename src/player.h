9909b35b145563dc7f8975b51b265dca4aff1498
Eric4044 <125145233+Eric4044@users.noreply.github.com>
2023-05-05 17:46:01 +0800

    Update player.h and add common

--- src/player.h
+++ src/player.h
@@ -1,17 +1,29 @@
 #ifndef PLAYER
-#define PLAYER
+#define PLAYER       //avoid multiple definitions
 
+//import
 #include "block.h"
 #include "gameMap.h"
 #include <map>
+
 class Map;
+
 class Player{
+
 public:
-    //Constructor  skin--std::move(std::map <char,int>)
+
+    //Constructor
+    //accept all parameters
+    //skin--std::move(std::map <char,int>)
     Player(char appearance_,int level_,int bombNum_,int wealth_,int damage_, int oxygen_, int depth_, int x_, int y_, std::map<char, int> skin_):
             appearance(appearance_), level(level_), bombNum(bombNum_), wealth(wealth_),damage(damage_),oxygen(oxygen_),depth(depth_),x(x_),y(y_),skin(skin_) {}
 
+    //default
+    //set the initial parameter
     Player(): Player('@',1,1,0,1,10,0,0,0,skinInt()) {}
+
+    //for loadFromFile
+    //accept appearance,level,bombNum,wealth,damage
     Player(char appearance_, int level_, int bombNum_, int wealth_,int damage_): Player(appearance_,level_,bombNum_,wealth_,damage_,10,0,0,0,skinInt()) {}
 
     //Destructor
@@ -19,17 +31,20 @@ public:
 
     //movement
 
-//    void move_left();
-//    void move_right();
-//    void move_down();
+    void move_left();
+    void move_right();
+    void move_down();
+
     void move_left(Map &mp);   //Judging whether to the edge
     void move_right(Map &mp);  //Judging whether to the edge
     void move_down(Map &mp);   //Judging whether to the edge
-	
-	//attack
-	bool attack(Block& target);
+
+    //attack
+    bool attack(Block& target);
+
     //bomb
     void bomb(Map &mp);
+
     //getters and setters
 
     int getWealth() const;
@@ -49,49 +64,65 @@ public:
     void setLocation(int x,int y);
 
     int getDepth() const;
-	void setDepth(int d);
-	
-	int getTime() const;
-	void setTime(int t);
-	
+    void setDepth(int d);
+
+    //time
+    //time used by timer
+    int getTime() const;
+    void setTime(int time);
+
     int getOxygen() const;
     void setOxygen(int oxygen);
 
     int getLevel() const;
     void setLevel(int level);
 
+
+    // get skin map
     std::map <char,int> getSkin() const;
+
+    //map to store the appearance
+    //pair.first store appearance
+    //pair.second store status
+    //0-> you don't have this skin
+    //1-> you have this skin
+    //2-> the skin you are wearing
     void setSkin(std::map <char,int> skin);
 
-    //Reset score, depth, x, y when starting a new game
+    //Reset wealth, depth, x, y when starting a new game
     void playerInitialization(Map &mp);
 
 private:
-    char appearance;
-    std::map <char,int> skin;
+
+    char appearance;          // the skin you are wearing
+    std::map <char,int> skin; // store skin
     int level;
-    int bombNum;
-    int wealth;
-    int damage;
+    int bombNum;     //how many bombs you have
+    int wealth;      //how much money you have
+    int damage;      //your attack power
     int oxygen;      //max time
     int depth;       //absolute depth
-    int x, y;        //location on the map
-	int time;		//time
-    void move(int);
+    int x;           //location(horizontal) on the map
+    int y;           //location(vertical) on the map
+    int time;		 //time
 
-    //skin initialization
+    void move(int);  //method move(private)
+
+    //skin initialization(static)
     static std::map<char, int> skinInt() {
 
-        std::map<char, int> TempMColor;
-        TempMColor.insert(std::pair<char, int>('@',1));
-        TempMColor.insert(std::pair<char, int>('#',0));
-        TempMColor.insert(std::pair<char, int>('$',0));
-        TempMColor.insert(std::pair<char, int>('%',0));
-        TempMColor.insert(std::pair<char, int>('^',0));
-        return TempMColor;
+        std::map<char, int> TempMColor;  //temp
+
+        //skin
+        TempMColor.insert(std::pair<char, int>('@',1));   //initial skinI(@)
+        TempMColor.insert(std::pair<char, int>('#',0));   //Purchasable(#) skins not owned
+        TempMColor.insert(std::pair<char, int>('$',0));   //Purchasable($) skins not owned
+        TempMColor.insert(std::pair<char, int>('%',0));   //Purchasable(%) skins not owned
+        TempMColor.insert(std::pair<char, int>('^',0));   //Purchasable(^) skins not owned
+
+        return TempMColor;                //return to Constructor
 
     }
-
 };
 
 #endif
