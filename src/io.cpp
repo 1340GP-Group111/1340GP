3f02b118ea2cea8479f8bc290f33eb11bc9767d3
duanxihan <117086781+duanxihan@users.noreply.github.com>
2023-05-05 18:06:28 +0800

    Add comments

--- src/io.cpp
+++ src/io.cpp
@@ -7,17 +7,17 @@
 long long encode(const std::string &s){         // Generate the hash code of the player info.
     long long sum = 0;
     for(auto c:s){
-        sum = (sum*HASH+c)%MOD;
+        sum = (sum*HASH+c)%MOD;      //use hash code to prtect the data
     }
     return sum;
 }
 
 int saveToFile(const Player& player){       // Save the player info into gameData.dat
-    std::ofstream out("./gameData.dat");
+    std::ofstream out("./gameData.dat");  //set up a dat file
     if(!out) return 1;              // Fail to open data file.
     auto wealth = player.getWealth(), bomb = player.getBombNum(),oxygen=player.getOxygen(),damage=player.getDamage();
     auto appearance = player.getAppearance();
-    out << wealth << "\n" << bomb << "\n" << appearance << "\n"<<damage<<"\n"<<oxygen;
+    out << wealth << "\n" << bomb << "\n" << appearance << "\n"<<damage<<"\n"<<oxygen;  //file output
 
 //    std::string info = std::to_string(wealth)+std::to_string(bomb)+appearance+std::to_string(damage)+std::to_string(oxygen);          // Join the items into a string
 //    long long sum = encode(info);           // Get the hash sum.
@@ -28,10 +28,13 @@ int saveToFile(const Player& player){       // Save the player info into gameDat
 }
 
 int loadFromFile(Player& player){
+    //open the file
     std::ifstream in("./gameData.dat");
     if(!in) return 1;           // Fail to open data file.
     int wealth, bomb, damage, oxygen;
+    //define variables to store the game data
     char appearance;
+    // uses char variable to store player's appearance
     long long data_sum, cur_sum;
     in >> wealth >> bomb >> appearance  >> damage >> oxygen >> data_sum;
 
@@ -40,16 +43,21 @@ int loadFromFile(Player& player){
 //    if(cur_sum != data_sum) return 2;           // Cannot pass hash validation. Data is incorrect.
 
     player.setWealth(wealth);
+    //load previuos wealth
     player.setBombNum(bomb);
+    //load previuos bomb numbers
     player.setAppearance(appearance);
+    //load previuos appearance
     player.setDamage(damage);
+    //load previuos damage
     player.setOxygen(oxygen);
+    //load previuos oxygen number
 
     return 0;           // Load successfully.
     
 }
 
-void flush(){
+void flush(){           // refresh the interface
     for(int i=0;i<50;i++){
         std::cout<<std::endl;
     }
